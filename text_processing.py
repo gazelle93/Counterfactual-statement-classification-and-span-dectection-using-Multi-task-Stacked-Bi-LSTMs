bca92d9d66141bbeee827d216e574ceb86ec334b
Mingyou Sung <mingyousung@gmail.com>
2022-06-01 15:36:42 -0400

    Update text_processing.py

--- text_processing.py
+++ text_processing.py
@@ -1,55 +1,86 @@
 import stanza
 import spacy
+from allennlp.modules.elmo import Elmo, batch_to_ids
 
-def get_nlp_pipeline(_nlp_pipeline):
+def get_model():
+    options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+    weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+
+    elmo = Elmo(options_file=options_file,
+                weight_file=weight_file,
+                num_output_representations=2,
+                dropout=0)
+
+    return elmo
+
+def get_wordembedding(embedding_model, datasamples):
+    embeddings = []
+    for datasample in datasamples:
+        elmo_input = batch_to_ids([datasample])
+        embeddings.append(embedding_model(elmo_input)['elmo_representations'][0])
+    return embeddings
+
+
+
+
+def get_nlp_pipeline(_nlp_pipeline, device):
     if _nlp_pipeline == "stanza":
-        return stanza.Pipeline('en')
+        if device:
+            return stanza.Pipeline('en', use_gpu= True)
+        else:
+            return stanza.Pipeline('en')
     elif _nlp_pipeline == "spacy":
+        if device:
+            spacy.prefer_gpu()
         return spacy.load("en_core_web_sm")
     
 
 def word_tokenization(_input_text, nlp, _nlp_pipeline=None, _lower=False):
-    if _lower == True:
-        _input_text = _input_text.lower()
-    
-    input_tk_list = []
-    input_pos_list = []
+    if _input_text != "{}":
+        if _lower == True:
+            _input_text = _input_text.lower()
 
-    if _nlp_pipeline == None:
-        return _input_text.split()
+        input_tk_list = []
+        input_pos_list = []
 
-    elif _nlp_pipeline == "stanza":
-        text = nlp(_input_text)
+        if _nlp_pipeline == None:
+            return _input_text.split()
 
-        for sen in text.sentences:
-            for tk in sen.tokens:
-                tk_info_dict = tk.to_dict()[0]
-                cur_tk = tk_info_dict["text"]
+        elif _nlp_pipeline == "stanza":
+            text = nlp(_input_text)
+
+            for sen in text.sentences:
+                for tk in sen.tokens:
+                    tk_info_dict = tk.to_dict()[0]
+                    cur_tk = tk_info_dict["text"]
+                    input_tk_list.append(cur_tk)
+                    input_pos_list.append(tk_info_dict["xpos"])
+            return input_tk_list, input_pos_list
+
+        elif _nlp_pipeline == "spacy":
+            text = nlp(_input_text)
+
+            for tk_idx, tk in enumerate(text):
+                cur_tk = tk.text
                 input_tk_list.append(cur_tk)
-                input_pos_list.append(tk_info_dict["xpos"])
-        return input_tk_list, input_pos_list
+                input_pos_list.append(tk.pos_)
 
-    elif _nlp_pipeline == "spacy":
-        text = nlp(_input_text)
-
-        for tk_idx, tk in enumerate(text):
-            cur_tk = tk.text
-            input_tk_list.append(cur_tk)
-            input_pos_list.append(tk.pos_)
-
-        return input_tk_list, input_pos_list
+            return input_tk_list, input_pos_list
+    return [], []
 
 
 def get_span_of_target(input_text, target_text):
-    for i in range(len(input_text)):
-        if input_text[i:i+len(target_text)] == target_text:
-            return [x for x in range(i,i+len(target_text))]
-
+    if target_text != []:
+        for i in range(len(input_text)):
+            if input_text[i:i+len(target_text)] == target_text:
+                return [x for x in range(i,i+len(target_text))]
+        return []
+    else:
+        return []
 
 def get_output_label(input_text, ant_span, con_span):
-    label_list = ["O", "A", "C"]
-
     output_label = ["O" for x in range(len(input_text))]
+
     for idx, _ in enumerate(input_text):
         if idx in ant_span:
             output_label[idx] = "A"
@@ -57,8 +88,7 @@ def get_output_label(input_text, ant_span, con_span):
             output_label[idx] = "C"
     return output_label
 
-def get_target(input_text, ant_span, con_span, nlp_pipeline):
-    selected_nlp_pipeline = get_nlp_pipeline(nlp_pipeline)
+def get_target(input_text, ant_span, con_span, selected_nlp_pipeline, nlp_pipeline):
 
     processed_cur_text, processed_cur_pos = word_tokenization(input_text, selected_nlp_pipeline, nlp_pipeline)
     processed_ant_span_text, processed_ant_pos = word_tokenization(ant_span, selected_nlp_pipeline, nlp_pipeline)
