179e31e6b86c2c9ce68f520f41c13a95db089555
Mingyou Sung <mingyousung@gmail.com>
2022-06-01 15:36:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,79 +6,94 @@ import torch.nn as nn
 from tqdm import tqdm
 import numpy as np
 
-from text_processing import get_target
-import my_onehot
+from text_processing import get_model, get_wordembedding
 from lstms import SequenceTagger
-from utils import feature_to_idx
+from utils import get_features, load_datasamples, get_processed_datasamples, feature_to_idx, get_evaluation_result
 
 def main(args):
-    cur_text = "However, he also doubts that a hacker would have much interest in the blood pressure readings you're sending to your doctor because if you are in his shoes, you'd find it difficult to make profit off that data."
-    ant_span_text = "if you are in his shoes"
-    con_span_text = "you'd find it difficult to make profit off that data"
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+        print("---Running on GPU.")
+    else:
+        device = torch.device('cpu')
+        print("---Running on CPU.")
 
-    tokens, pos_span_target, sequence_span_target = get_target(cur_text, ant_span_text, con_span_text,"spacy")
+    # load & preprocess dataset
+    print("---Initiating Dataset Loading & Pre-processing.")
+    train_dataset, test_dataset = load_datasamples(args.subtask)
+    train_dataset = get_processed_datasamples(train_dataset[:10], args.subtask)
+    test_dataset = get_processed_datasamples(test_dataset[:10], args.subtask)
 
-    # util part
-    pos_to_ix, r_pos_to_ix, tag_to_ix, r_tag_to_ix = feature_to_idx(pos_span_target, sequence_span_target)
+    train_datasamples = get_features(train_dataset, args.nlp_pipeline, args.subtask, device)
+    test_datasamples = get_features(test_dataset, args.nlp_pipeline, args.subtask, device)
 
-    # One-hot Encoding
-    embeddings = my_onehot.get_onehot_encoding([cur_text], cur_text, args.nlp_pipeline, args.unk_ignore)
+    pos_datasample = np.concatenate((train_datasamples[:,1], test_datasamples[:,1]))
+    pos_to_ix, r_pos_to_ix, tag_to_ix, r_tag_to_ix = feature_to_idx(pos_datasample, args.subtask)
+    print("---Done Dataset Loading & Pre-processing.")
 
-    emb_dim = embeddings.size()[1]
-    model = SequenceTagger(input_dim=emb_dim, hidden_dim=emb_dim, pos_to_ix=pos_to_ix, tag_to_ix=tag_to_ix)
+    # get ELMo word embedding model
+    print("---Initiating Word embedding (ELMo).")
+    embedding_model = get_model()
+    train_set = get_wordembedding(embedding_model, train_datasamples[:,0])
+    test_set = get_wordembedding(embedding_model, test_datasamples[:,0])
+    print("---Done Word embedding (ELMo).")
 
+    # model
+    emb_dim = train_set[0].size()[2]
+    model = SequenceTagger(input_dim=emb_dim, hidden_dim=emb_dim, pos_to_ix=pos_to_ix, tag_to_ix=tag_to_ix).to(device)
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
 
-
-
+    print("---Initiating training process.")
+    # training
     for epoch in range(args.num_epochs):
         model.train()
 
-        rnd = random.random()
-        returning_layer = round(rnd)
+        for idx in tqdm(range(len(train_set))):
+            model.zero_grad()
+            rnd = random.random()
+            returning_layer = round(rnd)
 
-        pred = model(embeddings, returning_layer)
+            pred = model(train_set[idx], returning_layer)
 
-        # predicting POS tags
-        if returning_layer == 0:
-            cur_target = torch.tensor([r_pos_to_ix[x] for x in pos_span_target])
-            loss = loss_function(pred, cur_target)
+            # predicting POS tags
+            if returning_layer == 0:
+                pred = pred.reshape(-1, len(pos_to_ix))
+                cur_target = torch.tensor([r_pos_to_ix[x] for x in train_datasamples[idx,1]])
+                loss = loss_function(pred, cur_target)
 
-        else:
-            cur_target = torch.tensor([r_tag_to_ix[x] for x in sequence_span_target])
-            loss = loss_function(pred, cur_target)
+            else:
+                pred = pred.reshape(-1, len(tag_to_ix))
+                cur_target = torch.tensor([r_tag_to_ix[x] for x in train_datasamples[idx,2]])
+                loss = loss_function(pred, cur_target)
 
-        loss.backward()
-        optimizer.step()
+            loss.backward(retain_graph=True)
+            optimizer.step()
 
 
+    # evaluation
     model.eval()
-    pred = model(embeddings, 1)
-    pred = [tag_to_ix[x] for x in np.argmax(pred.detach().numpy(), axis=1)]
 
-    print("Prediction: {}".format(pred))
-    print("Actual Target: {}".format(sequence_span_target))
+    prediction_list = []
+    gold_list = []
+    for idx in range(len(test_set)):
+        pred = model(test_set[idx], 1)
+        pred = pred.reshape(-1, len(tag_to_ix))
+        pred = [tag_to_ix [x] for x in np.argmax(pred.detach().numpy(), axis=1)]
 
-    a_match = 0
-    c_match = 0
-    for p, t in zip(pred, sequence_span_target):
-        if p == t =="A":
-            a_match+=1
-        elif p == t =="C":
-            c_match+=1
+        prediction_list.append(pred)
+        gold_list.append(test_datasamples[idx,2])
 
-    print("Antecedent Span Match: {} / {}" .format(a_match, len([x for x in sequence_span_target if x == "A"])))
-    print("Consequent Span Match: {} / {}" .format(c_match, len([x for x in sequence_span_target if x == "C"])))
+    get_evaluation_result(gold_list, prediction_list)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--subtask", default="2", help="The selection of subtask (1 or 2).")
     parser.add_argument("--nlp_pipeline", default="spacy", type=str, help="NLP preprocessing pipeline.")
-    parser.add_argument("--unk_ignore", default=True, help="Ignore unknown tokens.")
-    parser.add_argument("--learning_rate", default=1e-2, help="Learning rate.")
-    parser.add_argument("--num_epochs", default=100, help="Number of epochs for training.")
+    parser.add_argument("--learning_rate", default=1e-3, help="Learning rate.")
+    parser.add_argument("--num_epochs", default=5, help="Number of epochs for training.")
     args = parser.parse_args()
 
     main(args)
